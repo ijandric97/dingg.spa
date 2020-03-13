1eb8aa30bfae70806f087f81f881cae73e018dc4
Ivan Jandric <ijandric@iolap.com>
2020-03-13 15:40:26 +0100

    Added palx module

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import * as palx from 'palx';
+
+console.log(palx.default("white"));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
